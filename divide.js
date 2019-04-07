f30405d764af56c9c65b46b687e5c7e498cc7dbe
Shaik Aqib <shaik@L-156179465.broadband>
2019-04-07 16:35:18 +0100

    commiting divide.js, the only file probably left for us.

--- divide.js
+++ divide.js
@@ -1,4 +1,7 @@
 // divide.js
+
+//Allright, I think I made a boo-boo and hence we have to modify the divide 
+//functionality as well to do proper cherry-picking another time. Here we go.
 function divide(a, b) {
     // TODO return
 };
