1e31c62dab0756b4e265a4b0b7d552ad55cf034c
Shaik Aqib <shaik@L-156179465.broadband>
2019-04-07 13:46:35 +0100

    just some sample comments added so as to learn and hands-on git commands

--- divide.js
+++ divide.js
@@ -1,6 +1,7 @@
 // divide.js
 
-//return divison of two numbers
+//return divison of two numbers 
+//some sample functionality just to check the git features
 function divide(a, b) {
     // TODO return
 };
