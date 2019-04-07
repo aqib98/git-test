7975a0af94bf7eb0e3160a4dfadf5255a6ffb6b4
Shaik Aqib <shaik@L-156179465.broadband>
2019-04-07 16:45:32 +0100

    commiting new name for add

--- add.js
+++ add.js
@@ -3,7 +3,7 @@
 //add functionality a new change added as part of this commit so that 
 //i can merge dev into master and see if there are any changes while doing so
 //so .... here we are....
-function add(a, b) {
+function add11(a, b) {
     // TODO return
     return a+b
 };
