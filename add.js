3c29801293a017ef4483d8527a3bb935d1eb3e67
Shaik Aqib <shaik@L-156179465.broadband>
2019-04-07 16:21:14 +0100

    adding the changes made to the add.js file to remote reposiotory

--- add.js
+++ add.js
@@ -1,4 +1,8 @@
 // add.js
+
+//add functionality a new change added as part of this commit so that 
+//i can merge dev into master and see if there are any changes while doing so
+//so .... here we are....
 function add(a, b) {
     // TODO return
     return a+b
